d7faf01a53d8c7fa8c1f49ebd11632962f46925f
Nicolas Harlem Eide <nicolas@harlemeide.net>
2021-10-11 21:08:20 +0200

    Add cluster and log groups as outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "trigger_lambda_arn" {
 output "artifact_bucket_arn" {
     value = aws_s3_bucket.artifacts.arn
 }
+
+output "cluster_arn" {
+    value = aws_ecs_cluster.cluster.arn
+}
+
+output "ecs_log_group_arn" {
+    value = aws_cloudwatch_log_group.ecs.arn
+}
+
+output "lambda_log_group_arn" {
+    value = aws_cloudwatch_log_group.lambda.arn
+}
