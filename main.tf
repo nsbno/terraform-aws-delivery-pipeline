1771cd47f098b4638c00190a0b5667f1badf78a2
Nicolas Harlem Eide <nicolas@harlemeide.net>
2021-12-01 21:35:58 +0100

    Add requirement for AWS provider 3.55.0
    This is required because we're using Python 3.9 for lambdas.
    Earlier versions of the provider didn't support this runtime

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 terraform {
     required_version = ">= 1.0.0"
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 3.55.0"
+        }
+    }
 }
 
 data "aws_region" "current" {}
